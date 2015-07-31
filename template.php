ab649a2f8aa78ce9a17207af53d677a9eb5e22bf
adishap <porwaladisha@gmail.com>
2015-07-31 10:50:32 +0530

    template for UI added.

---
+++ template.php
@@ -0,0 +1,32 @@
+<!--This is template page you can directly move on to content of this page header, css_links and js_links added.-->
+
+<html>
+	<head>
+	
+		<?php 
+		//including CSS Links for page
+		include 'css_links.php';
+		?>
+
+		<title>School Of Commerce</title>
+
+	</head>
+
+	<body>
+		<?PHP 
+		//include header
+		include 'header.php';
+		?>
+<!--Add Content here-->		
+		
+		<?php
+		//including footer
+		include 'footer.php';
+		?>
+
+		<?php 
+		//including all the js files
+		include 'js_links.php';
+		?>
+	</body>
+</html>
